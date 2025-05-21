132c4eeecaec459cb427e1ae0cc9efd59db43d64
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2025-05-21 18:25:34 +1000

    :cop: tests/test_apps.py working

--- tests/test_apps.py
+++ tests/test_apps.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 from unittest.mock import patch
 import pytest
-from click.testing import CliRunner
+from typer.testing import CliRunner
 import torchapp as ta
 
 
 def test_model_defaults_change():
     class DummyApp(ta.TorchApp):
+        @ta.method
         def model(self, size: int = ta.Param(default=2)):
             assert size == 2
 
@@ -18,34 +19,18 @@ def test_model_unimplemented_error():
         ta.TorchApp().model()
 
 
-def test_dataloaders_unimplemented_error():
+def test_data_unimplemented_error():
     with pytest.raises(NotImplementedError):
-        ta.TorchApp().dataloaders()
+        ta.TorchApp().data()
 
 
-def test_assert_initialized():
-    class DummyApp(ta.TorchApp):
-        def __init__(self):
-            pass
-
-    with pytest.raises(TorchAppInitializationError):
-        DummyApp().cli()
-
-
-def test_click():
-    cli = ta.TorchApp.click()
+def test_main_help():
+    app = ta.TorchApp().main_app
     runner = CliRunner()
-    result = runner.invoke(cli, ['--help'])
+    result = runner.invoke(app, ['--help'])
     assert result.exit_code == 0
-    assert '[OPTIONS] COMMAND [ARGS]' in result.output
-
-
-def test_str():
-    class DummyApp(ta.TorchApp):
-        pass
-
-    app = DummyApp()
-    assert str(app) == "DummyApp"
+    assert "Show this message and exit." in result.output
+    assert 'Usage: predict [OPTIONS]' in result.output
 
 
 def test_checkpoint_default():
