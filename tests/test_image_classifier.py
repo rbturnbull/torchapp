a90fddad422aee48154cbadbe33e8c13158800f3
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2025-05-30 09:12:15 +1000

    :cop: adding test for get_image_paths

--- tests/test_image_classifier.py
+++ tests/test_image_classifier.py
@@ -1,6 +1,6 @@
 import pytest
 from torchapp.testing import TorchAppTestCase
-from torchapp.examples.image_classifier import ImageClassifier
+from torchapp.examples.image_classifier import ImageClassifier, get_image_paths
 
 
 class TestImageClassifier(TorchAppTestCase):
@@ -10,3 +10,12 @@ class TestImageClassifier(TorchAppTestCase):
         app = self.get_app()
         with pytest.raises(ValueError):
             app.model(model_name="resnet1000")
+
+
+    def test_get_image_paths(self):
+        expected_dir = self.get_expected_dir()
+        paths = get_image_paths(expected_dir)
+        assert len(paths) == 3
+        names = [p.name for p in paths]
+        assert 'vanjari-banner.jpg' in names
+        assert 'bloodhound.png' in names
